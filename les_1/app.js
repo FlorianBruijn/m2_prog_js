06e98b091e093452ff6eae0e90e5747475345f47
florian <34652@ma-web.nl>
2022-11-22 13:06:00 +0100

    1.0

---
+++ les_1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        let maam = "eerste code"
+        let appNaam = "website flo"
+        let versienummer = 0.9
+        let versiedatum = Date
+        let autheur = "florian"
+        let copyright = "florian"
+        let distributeur = "florian"
+        let darkmode = treu
+        console.log(naam)
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(distributeur)
+        console.log(darkmode)
+
+    }
+}
+
+let app = new App();
+app.runApplication
